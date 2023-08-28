b4393693db728bf7a427fac05938eb1bee3cf086
RoboXGamer <roboxgamer52@gmail.com>
2023-08-28 19:30:20 +0530

    Basic Implementation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,57 @@
+import { useState } from "react";
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
+	const [color, setcolor] = useState("grey");
+
 	return (
 		<>
-			<h1 className="font-mono text-5xl font-bold">Hello Tailwind!</h1>
+			<main
+				className="w-full h-screen bg-[#444] text-xl"
+				style={{ backgroundColor: color }}
+			>
+				<div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 mx-12 ">
+					<div className="flex flex-wrap gap-5 justify-center p-2 bg-slate-400 text-white shadow-lg rounded">
+						<Button
+							onClick={() => setcolor("white")}
+							bgColor="white"
+							textColor="black"
+							text="White"
+						></Button>
+						<Button
+							onClick={() => setcolor("black")}
+							bgColor="black"
+							text="Black"
+						></Button>
+						<Button
+							onClick={() => setcolor("red")}
+							bgColor="red"
+							text="Red"
+						></Button>
+						<Button
+							onClick={() => setcolor("green")}
+							bgColor="green"
+							text="Green"
+						></Button>
+						<Button
+							onClick={() => setcolor("blue")}
+							bgColor="blue"
+							text="Blue"
+						></Button>
+						<Button
+							onClick={() => setcolor("olive")}
+							bgColor="olive"
+							text="Olive"
+						></Button>
+						<Button
+							onClick={() => setcolor("coral")}
+							bgColor="coral"
+							text="Coral"
+						></Button>
+					</div>
+				</div>
+			</main>
 		</>
 	);
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+function Button({ textColor = "white", bgColor, text, onClick }) {
+	return (
+		<button
+			onClick={onClick}
+			className="py-1 px-3 rounded border-none hover:shadow-md"
+			style={{ backgroundColor: bgColor, color: textColor }}
+		>
+			{text}
+		</button>
+	);
+}
+
+export default Button;
